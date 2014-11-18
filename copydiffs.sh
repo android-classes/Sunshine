4dfc0ac56da1b828991686c8bd477853991e1dfe
Ralph Yozzo <ralph@brooklynmarathon.com>
2014-11-18 11:55:50 -0500

    add tablet

--- copydiffs.sh
+++ copydiffs.sh
@@ -1,3 +1,9 @@
  # must be in the udacity source directory
+
+ # sometimes you have to create directory and file first when it does not exist
+ # mkdir -p /home/ralph/AndroidStudioProjects/Sunshine/./app/src/main/res/layout-sw600dp
+ # touch /home/ralph/AndroidStudioProjects/Sunshine/./app/src/main/res/layout-sw600dp/activity_main.xml
+
+ #
  find . -name "*.java" -exec diff -q  {} ~/AndroidStudioProjects/Sunshine/{} \;  |sed "s/^Files/cp/" |sed "s/ and / /" |sed "s/differ//" |bash
  find . -name "*.xml" -exec diff -q  {} ~/AndroidStudioProjects/Sunshine/{} \;  |sed "s/^Files/cp/" |sed "s/ and / /" |sed "s/differ//" |bash
